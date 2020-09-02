669af9fdcf3e5720ab478a459bbf4fa0627ce96e
Nikhil Bhoski <nbhoski@bgl-nbhoski-deb9-64>
2020-09-02 14:21:53 +0530

    Updated copyright in new error class

--- src/main/java/com/mathworks/ci/MatlabNotFoundError.java
+++ src/main/java/com/mathworks/ci/MatlabNotFoundError.java
@@ -1,12 +1,14 @@
 package com.mathworks.ci;
 
+/**
+ * Copyright 2020 The MathWorks, Inc.
+ * 
+ */
+
 import java.io.FileNotFoundException;
 
 public class MatlabNotFoundError extends Error {
-   
-    /**
-     * 
-     */
+
     private static final long serialVersionUID = 7918595075502022644L;
 
     MatlabNotFoundError(String errorMessage){
