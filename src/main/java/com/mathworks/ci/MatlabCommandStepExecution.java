9cc91973d406057972fc78b8cc1a19e3483986e0
abhargav-mw <abhargav@mathworks.com>
2021-05-19 14:08:21 +0530

    Address Abhisheks review comments

--- src/main/java/com/mathworks/ci/MatlabCommandStepExecution.java
+++ src/main/java/com/mathworks/ci/MatlabCommandStepExecution.java
@@ -45,13 +45,12 @@ public class MatlabCommandStepExecution extends SynchronousNonBlockingStepExecut
         
         int exitCode = execMatlabCommand(workspace, launcher, listener, env);
 
-        if(exitCode == 0){
-            getContext().setResult(Result.SUCCESS);
-            return null;
+        if(exitCode != 0){
+            // throw an exception if return code is non-zero
+            stop(new MatlabExecutionException(exitCode));
         }
 
-        // throw an exception if return code is non-zero
-        stop(new MatlabExecutionException(exitCode));
+        getContext().setResult(Result.SUCCESS);
         return null;
     }
 
