e3431e9d08fbf587f6485c6c0374e019069648eb
Vahila <70003902+Vahila@users.noreply.github.com>
2023-01-03 11:02:09 +0530

    Remove build wrapper and matrix tests

--- src/integ-test/java/com/mathworks/ci/RunMATLABCommandIT.java
+++ src/integ-test/java/com/mathworks/ci/RunMATLABCommandIT.java
@@ -64,8 +64,8 @@ public class RunMATLABCommandIT {
 
     @Test
     public void verifyBuildFailureWhenMatlabCommandFails() throws Exception {
-        this.buildWrapper.setMatlabBuildWrapperContent(new MatlabBuildWrapperContent(Message.getValue("matlab.custom.location"), MatlabRootSetup.getMatlabRoot()));
-        project.getBuildWrappersList().add(this.buildWrapper);
+//         this.buildWrapper.setMatlabBuildWrapperContent(new MatlabBuildWrapperContent(Message.getValue("matlab.custom.location"), MatlabRootSetup.getMatlabRoot()));
+//         project.getBuildWrappersList().add(this.buildWrapper);
         RunMatlabCommandBuilder tester =
                 new RunMatlabCommandBuilder();
         tester.setMatlabCommand(TestData.getPropValues("pwd"));
@@ -91,7 +91,7 @@ public class RunMATLABCommandIT {
     /*
      * Test to verify if Matrix build fails when MATLAB is not available.
      */
-    @Test
+ 
     public void verifyMatrixBuildFails() throws Exception {
         MatrixProject matrixProject = jenkins.createProject(MatrixProject.class);
         Axis axes = new Axis("VERSION", "R2018a", "R2018b");
@@ -116,7 +116,7 @@ public class RunMATLABCommandIT {
     /*
      * Test to verify if Matrix build passes .
      */
-    @Test
+  
     public void verifyMatrixBuildPasses() throws Exception {
         MatrixProject matrixProject = jenkins.createProject(MatrixProject.class);
         String MATLABVersion1 = TestData.getPropValues("matlab.version");
