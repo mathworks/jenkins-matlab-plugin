c577b12054068cee0f0b92987dc111f603390ab6
David Buzinski <103441853+davidbuzinski@users.noreply.github.com>
2023-01-11 10:00:54 -0500

    Run build (#231)
    initial Run MATLAB Build implementation

---
+++ src/main/java/com/mathworks/ci/MatlabBuildStepExecution.java
@@ -0,0 +1,107 @@
+package com.mathworks.ci;
+
+/**
+ * Copyright 2022 The MathWorks, Inc.
+ *  
+ */
+
+import java.io.IOException;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution;
+import hudson.EnvVars;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.Launcher.ProcStarter;
+import hudson.model.Result;
+import hudson.model.TaskListener;
+
+public class MatlabBuildStepExecution extends SynchronousNonBlockingStepExecution<Void> implements MatlabBuild {
+    
+    private static final long serialVersionUID = 4771831219402275744L;
+    
+    private String tasks;
+
+    public MatlabBuildStepExecution(StepContext context, String tasks) {
+        super(context);
+        this.tasks = tasks;
+    }
+
+    private String getTasks() {
+        return this.tasks;
+    }
+
+    @Override
+    public Void run() throws Exception {
+        final Launcher launcher = getContext().get(Launcher.class);
+        final FilePath workspace = getContext().get(FilePath.class);
+        final TaskListener listener = getContext().get(TaskListener.class);
+        final EnvVars env = getContext().get(EnvVars.class);
+        
+        //Make sure the Workspace exists before run
+        
+        workspace.mkdirs();
+        
+        int exitCode = execMatlabCommand(workspace, launcher, listener, env);
+
+        if(exitCode != 0){
+            // throw an exception if return code is non-zero
+            stop(new MatlabExecutionException(exitCode));
+        }
+
+        getContext().setResult(Result.SUCCESS);
+        return null;
+    }
+
+    @Override
+    public void stop(Throwable cause) throws Exception {
+        getContext().onFailure(cause);
+    }
+    
+    private int execMatlabCommand(FilePath workspace, Launcher launcher,
+            TaskListener listener, EnvVars envVars) throws IOException, InterruptedException {
+        final String uniqueTmpFldrName = getUniqueNameForRunnerFile();  
+        final String uniqueBuildFile =
+                "build_" + getUniqueNameForRunnerFile().replaceAll("-", "_");
+        final FilePath uniqueTmpFolderPath =
+                getFilePathForUniqueFolder(launcher, uniqueTmpFldrName, workspace);
+
+        // Create MATLAB script
+        createMatlabScriptByName(uniqueTmpFolderPath, uniqueBuildFile, workspace, listener);
+        ProcStarter matlabLauncher;
+
+        try {
+            matlabLauncher = getProcessToRunMatlabCommand(workspace, launcher, listener, envVars,
+                    "cd('"+ uniqueTmpFolderPath.getRemote().replaceAll("'", "''") +"'); "+ uniqueBuildFile, uniqueTmpFldrName);
+            listener.getLogger()
+                    .println("#################### Starting command output ####################");
+            return matlabLauncher.pwd(workspace).join();
+
+        } finally {
+            // Cleanup the tmp directory
+            if (uniqueTmpFolderPath.exists()) {
+                uniqueTmpFolderPath.deleteRecursive();
+            }
+        }
+    }
+    
+    private void createMatlabScriptByName(FilePath uniqeTmpFolderPath, String uniqueScriptName, FilePath workspace, TaskListener listener) throws IOException, InterruptedException {
+
+        // Create a new command runner script in the temp folder.
+        final FilePath matlabBuildFile =
+                new FilePath(uniqeTmpFolderPath, uniqueScriptName + ".m");
+        final String tasks = getContext().get(EnvVars.class).expand(getTasks());
+        String cmd = "buildtool";
+
+        if (!tasks.trim().isEmpty()) {
+            cmd += " " + tasks;
+        }
+        final String matlabBuildFileContent =
+                "cd '" + workspace.getRemote().replaceAll("'", "''") + "';\n" + cmd;
+
+        // Display the commands on console output for users reference
+        listener.getLogger()
+                .println("Generating MATLAB script with content:\n" + cmd + "\n");
+
+        matlabBuildFile.write(matlabBuildFileContent, "UTF-8");
+    }
+}
---
+++ src/main/java/com/mathworks/ci/RunMatlabBuildStep.java
@@ -0,0 +1,63 @@
+package com.mathworks.ci;
+
+/**
+ * Copyright 2022 The MathWorks, Inc.
+ *  
+ */
+
+import java.util.Set;
+import org.jenkinsci.plugins.workflow.steps.Step;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import com.google.common.collect.ImmutableSet;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.Util;
+
+public class RunMatlabBuildStep extends Step {
+
+    private String tasks;
+
+    @DataBoundConstructor
+    public RunMatlabBuildStep(String tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getTasks() {
+        return Util.fixNull(tasks);
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        return new MatlabBuildStepExecution(context, getTasks());
+    }
+
+    @Extension
+    public static class CommandStepDescriptor extends StepDescriptor {
+
+        @Override
+        public Set<? extends Class<?>> getRequiredContext() {
+            return ImmutableSet.of(TaskListener.class, FilePath.class, Launcher.class,
+                    EnvVars.class, Run.class);
+        }
+
+        @Override
+        public String getFunctionName() {
+            return Message.getValue("matlab.build.build.step.name");
+        }
+        
+        @Override
+        public String getDisplayName() {
+            return Message.getValue("matlab.build.step.display.name");
+        }
+    }
+}
+
+
