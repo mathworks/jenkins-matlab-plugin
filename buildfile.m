d9f323f0a87af219234b129eb53884d4201ef896
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2023-11-08 15:26:26 +0530

    Added plugin files

---
+++ buildfile.m
@@ -0,0 +1,29 @@
+function plan = buildfile
+% Copyright 2023 The MathWorks, Inc.
+
+import matlab.buildtool.tasks.*
+import matlab.buildtool.Task;
+
+plan = buildplan(localfunctions);
+
+plan("check") = Task;
+plan("check").Actions = @checkTask;
+plan("test") = Task;
+plan("test").Actions = @testTask;
+plan("test").Dependencies = ["check","show"];
+plan("show") = Task;
+plan("show").Actions = @showTask;
+
+%plan("dsplay")
+
+plan("clean") = CleanTask();
+plan.DefaultTasks = ["test"];
+
+function checkTask(~)
+ % Identify code issues
+disp("Hello Check");
+end
+function showTask(~)
+ % Identify code issues
+disp("Hello Show");
+end
