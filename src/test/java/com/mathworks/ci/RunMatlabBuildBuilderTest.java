f23b9429b7a9d8af3d0cbb877404b19659638f59
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-03-19 21:44:31 +0530

    Dev main (#301)
    * 2.11.2 snapshot qualification (#296)
    
    * Fixed issue 281 (#284)
    
    * Update the maven release  (#286)
    
    * [maven-release-plugin] prepare release matlab-2.12.0
    
    * [maven-release-plugin] prepare for next development iteration
    
    * Added summary page warnning
    
    * Updated summary warnning
    
    * Fixed Summary page issues
    
    * Fixed issue 292
    
    * Fixed 289 as per comment
    
    * Fixed error message issue 288
    
    * Fixed 289
    
    * Add build options (#294)
    
    * add build options
    
    * update gitignore
    
    * add tests for build options
    
    * add fixes and more tests
    
    * fix tests
    
    * modify help text
    
    * fix help text
    
    * minor text fix
    
    * fix null error and formatting
    
    * remove buildOptions check from EnvVarSupport test
    
    * add env variables support for build options and formatting fixes
    
    * minor fixes
    
    * documenting MATLAB build options and MATLAB build result
    
    * Fixed issue 298 (#300)
    
    ---------
    
    Co-authored-by: Tushar Sharma <150027500+tsharmaMW@users.noreply.github.com>
    Co-authored-by: mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>

--- src/test/java/com/mathworks/ci/RunMatlabBuildBuilderTest.java
+++ src/test/java/com/mathworks/ci/RunMatlabBuildBuilderTest.java
@@ -223,6 +223,21 @@ public class RunMatlabBuildBuilderTest {
     }
 
     /*
+     * Test to verify builder correctly sets build options that user entered.
+     */
+    @Test
+    public void verifyBuildPicksTheCorrectBuildOptions() throws Exception {
+        this.buildWrapper.setMatlabBuildWrapperContent(new MatlabBuildWrapperContent(Message.getValue("matlab.custom.location"), getMatlabroot("R2018b")));
+        project.getBuildWrappersList().add(this.buildWrapper);
+        scriptBuilder.setTasks("");
+        scriptBuilder.setBuildOptions(new BuildOptions("-continueOnFailure -skip compile"));
+        project.getBuildersList().add(this.scriptBuilder);
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        jenkins.assertLogContains("Generating MATLAB script with content", build);
+        jenkins.assertLogContains("-continueOnFailure -skip compile", build);
+    }
+
+    /*
      * Test to verify if MATLAB scratch file is not generated in workspace for this builder.
      */
     @Test
@@ -244,13 +259,16 @@ public class RunMatlabBuildBuilderTest {
         EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
         EnvVars var = prop.getEnvVars();
         var.put("TASKS", "compile");
+        var.put("BUILD_OPTIONS", "-continueOnFailure -skip test");
         jenkins.jenkins.getGlobalNodeProperties().add(prop);
         this.buildWrapper.setMatlabBuildWrapperContent(new MatlabBuildWrapperContent(Message.getValue("matlab.custom.location"), getMatlabroot("R2018b")));
         project.getBuildWrappersList().add(this.buildWrapper);
         scriptBuilder.setTasks("$TASKS");
+        scriptBuilder.setBuildOptions(new BuildOptions("$BUILD_OPTIONS"));
         project.getBuildersList().add(scriptBuilder);
         FreeStyleBuild build = project.scheduleBuild2(0).get();
         jenkins.assertLogContains("compile", build);
+        jenkins.assertLogContains("-continueOnFailure -skip test", build);
     }
     
     /*
