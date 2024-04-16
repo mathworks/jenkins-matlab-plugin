748264756ba9d2ca0c10c5700cda0e9363582ab4
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-04-16 15:17:27 +0530

    UPdated the fID logic

--- src/main/resources/+ciplugins/+jenkins/BuildReportPlugin.m
+++ src/main/resources/+ciplugins/+jenkins/BuildReportPlugin.m
@@ -6,26 +6,24 @@ classdef BuildReportPlugin < matlab.buildtool.plugins.BuildRunnerPlugin
 
         function runTaskGraph(plugin, pluginData)
             runTaskGraph@matlab.buildtool.plugins.BuildRunnerPlugin(plugin, pluginData);
-            [fID, msg] = fopen(fullfile(getenv("WORKSPACE"),'.matlab/buildArtifact.json'), 'w');
+            [fID, msg] = fopen(fullfile(pwd,'buildArtifact.json'), 'w');
 
             if fID == -1
                 warning('BuildTool:artifactFileWarnning','Could not open a file for Jenkins build result table due to: %s', msg);
-                fID = 1;
+            else
+                taskDetails = struct();
+                for idx = 1:numel(pluginData.TaskResults)
+                    taskDetails(idx).name = pluginData.TaskResults(idx).Name;
+                    taskDetails(idx).description = pluginData.TaskGraph.Tasks(idx).Description;
+                    taskDetails(idx).failed = pluginData.TaskResults(idx).Failed;
+                    taskDetails(idx).skipped = pluginData.TaskResults(idx).Skipped;
+                    taskDetails(idx).duration = string(pluginData.TaskResults(idx).Duration);
+               end
+               a = struct("taskDetails",taskDetails);
+               s = jsonencode(a,"PrettyPrint",true);
+               fprintf(fID, '%s',s);
+               fclose(fID);
             end
-
-            taskDetails = struct();
-            for idx = 1:numel(pluginData.TaskResults)
-                taskDetails(idx).name = pluginData.TaskResults(idx).Name;
-                taskDetails(idx).description = pluginData.TaskGraph.Tasks(idx).Description;
-                taskDetails(idx).failed = pluginData.TaskResults(idx).Failed;
-                taskDetails(idx).skipped = pluginData.TaskResults(idx).Skipped;
-                taskDetails(idx).duration = string(pluginData.TaskResults(idx).Duration);
-            end
-            a = struct("taskDetails",taskDetails);
-            s = jsonencode(a,"PrettyPrint",true);
-            fprintf(fID, '%s',s);
-            fclose(fID);
         end
-
     end
 end
